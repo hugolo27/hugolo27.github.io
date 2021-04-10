e878cadf7de1dc04e1ea4afab82b9ec8a735602b
hugolo27 <hugo33lopez@hotmail.com>
2021-04-09 20:14:39 -0400

    First commit

---
+++ static/js/translate.js
@@ -0,0 +1,327 @@
+const content = {
+    "home": {
+        "en": "Home",
+        "es": "Inicio",
+    },
+    "about": {
+        "en": "About",
+        "es": "Acerca de mi",
+    },
+    "about_me": {
+        "en": "About me",
+        "es": "Acerca de mi",
+    },
+    "bio": {
+        "en": "Bio",
+        "es": "Perfil",
+    },
+    "services": {
+        "en": "Services",
+        "es": "Servicios",
+    },
+    "portfolio": {
+        "en": "Portfolio",
+        "es": "Proyectos",
+    },
+    "technologies": {
+        "en": "Technologies",
+        "es": "Tecnologías",
+    },
+    "experience": {
+        "en": "Experience",
+        "es": "Experience",
+    },
+    "formation": {
+        "en": "Formation",
+        "es": "Académico",
+    },
+    "language": {
+        "en": "Language",
+        "es": "Idioma",
+    },
+    "spanish": {
+        "en": "Spanish",
+        "es": "Español",
+    },
+    "english": {
+        "en": "English",
+        "es": "Inglés",
+    },
+    "hello_world": {
+        "en": "Hello world!",
+        "es": "¡Hola mundo!",
+    },
+    "salute": {
+        "en": "I'm Hugo Lopez,<br/>Software developer",
+        "es": "Soy Hugo López,<br/>Desarrollador de software",
+    },
+    "connect": {
+        "en": "Let's connect!",
+        "es": "¡Conectémonos!",
+    },
+    "from": {
+        "en": "From",
+        "es": "De",
+    },
+    "from_c": {
+        "en": "Asuncion, Paraguay",
+        "es": "Asunción, Paraguay",
+    },
+    "living": {
+        "en": "Living in",
+        "es": "Viviendo en",
+    },
+    "birthday": {
+        "en": "Birthday",
+        "es": "Cumpleaños",
+    },
+    "birthday_c": {
+        "en": "April 27<sup>th</sup>",
+        "es": "27 de abril",
+    },
+    "languages": {
+        "en": "Languages",
+        "es": "Idiomas",
+    },
+    "languages_c": {
+        "en": "Spanish, english",
+        "es": "Español, inglés",
+    },
+    "favorite": {
+        "en": "Favorite sport",
+        "es": "Deporte favorito",
+    },
+    "favorite_c": {
+        "en": "Soccer",
+        "es": "Fútbol",
+    },
+    "my_bio": {
+        "en": "My Bio",
+        "es": "Mi Perfil",
+    },
+    "my_bio_c": {
+        "en": "With 4+ years of experience in software development, mainly focused on web and mobile platforms, I seek to broaden my horizon and constantly innovate while creating software solutions.<br/>" +
+            "<br/>" +
+            "My focus is on the legibility, simplicity, efficiency and elegance of the code in order to inspire my collaborators to follow patterns that allow creating optimal projects.<br/>" +
+            "<br/>" +
+            "I cordially invite you to know my profile and my work, thank you for visiting!",
+        "es": "Con más de 4 años de experiencia en desarrollo de software, enfocado principalmente en plataformas web y mobile, busco ampliar mi horizonte e innovar constantemente mientras creo soluciones de software.<br/>" +
+            "<br/>" +
+            "Mi enfoque está en la legibilidad, sencillez, eficiencia y elegancia del código para inspirar a mis colaboradores a seguir patrones que permitan crear proyectos óptimos.<br/>" +
+            "<br/>" +
+            "Te invito cordialmente a conocer mi perfil y mi trabajo, ¡gracias por tu visita!",
+    },
+    "web_development": {
+        "en": "Web development",
+        "es": "Desarrollo web",
+    },
+    "mobile_development": {
+        "en": "Mobile development",
+        "es": "Desarrollo mobile",
+    },
+    "consulting": {
+        "en": "Consulting",
+        "es": "Consultoría",
+    },
+    "technical_service": {
+        "en": "Technical service",
+        "es": "Servicio técnico",
+    },
+    "utility_app": {
+        "en": "Utility app",
+        "es": "App utilitaria",
+    },
+    "e_commerce": {
+        "en": "E-commerce website",
+        "es": "Sitio web e-commerce",
+    },
+    "management": {
+        "en": "Management software",
+        "es": "Sistema de gestión",
+    },
+    "native_android": {
+        "en": "Native Android",
+        "es": "Android nativo",
+    },
+    "programming_languages": {
+        "en": "Programming languages",
+        "es": "Lenguajes de programación",
+    },
+    "database_engines": {
+        "en": "Database engines",
+        "es": "Motores de base de datos",
+    },
+    "web_and_mobile": {
+        "en": "Web and mobile frameworks",
+        "es": "Frameworks web y mobile",
+    },
+    "web_front": {
+        "en": "Web front-end",
+        "es": "Frontend web",
+    },
+    "teamwork": {
+        "en": "Teamwork environment",
+        "es": "Entorno de trabajo en equipo",
+    },
+    "utilities": {
+        "en": "Utilities & Others",
+        "es": "Utilidades y otros",
+    },
+    "j1p": {
+        "en": "January 2021 - present",
+        "es": "Enero 2021 - presente",
+    },
+    "j1t": {
+        "en": "Software Developer",
+        "es": "Desarrollador de software",
+    },
+    "j1c": {
+        "en": "Front-end development of utility web applications for the company's customer service with React (HTML5, CSS3, Bootstrap 4, axios, react-router, redux).",
+        "es": "Desarrollo frontend de aplicaciones web utilitarias para el servicio de atención al cliente de la empresa con React(HTML5, CSS3, Bootstrap 4, axios, react-router, redux).",
+    },
+    "j2p": {
+        "en": "May 2020 - December 2020",
+        "es": "Mayp 2020 - Diciembre 2020",
+    },
+    "j2c": {
+        "en": "Full stack development of applications for internal company use. Procedures, functions, packages, triggers, tables and others of Oracle Database with PL / SQL language. Socket / RESTful connection APIs in Java language, RESTful API in javascript. Utility scripting in python, PHP, shell. UI / UX renovation of projects with HTML5 + CSS3 + Bootstrap + jQuery. ",
+        "es": "Desarrollo full stack de aplicaciones para uso interno de la empresa. Procedimientos, funciones, paquetes, triggers, tablas y otros de Base de datos Oracle con lenguaje PL/SQL. APIs de conexión socket/RESTful en lenguaje Java, API RESTful en javascript. Scripting de utilitarios en python, PHP, shell. Renovación de UI/UX de proyectos con HTML5 + CSS3 + Bootstrap + jQuery. ",
+    },
+    "j3p": {
+        "en": "December 2019 - March 2020",
+        "es": "Diciembre 2019 - Marzo 2020",
+    },
+    "j3t": {
+        "en": "Club Lounge Attendant",
+        "es": "Club Lounge Attendant"
+    },
+    "j3c": {
+        "en": "Rresponsible for helping to set tables, deliver food / drink orders, and additional kitchen / busser duties as needed, refresh food offerings during the day for VIP guests, responsible for engaging and attending to VIP guest needs.",
+        "es": "Rresponsable de ayudar a poner las mesas, entregar pedidos de comida / bebida y tareas adicionales de cocina / ayudante según sea necesario, refrescar las ofertas de comida durante el día para los invitados VIP, responsable de involucrar y atender las necesidades de los huéspedes VIP. ",
+    },
+    "j4p": {
+        "en": "October 2018 - November 2019",
+        "es": "Octubre 2018 - Noviembre 2019",
+    },
+    "j5p": {
+        "en": "April 2018 - October 2018",
+        "es": "Abril 2018 - Octubre 2018",
+    },
+    "j5c": {
+        "en": "Development of small e-commerce platforms with Django + jQuery and ASP .NET + Razor. Project UI improvement implementing Bootstrap 4 ",
+        "es": "Desarrollo de plataformas e-commerce de porte pequeño con Django + jQuery y ASP .NET + Razor. Mejoramiento de UI de proyectos implementando Bootstrap 4",
+    },
+    "j6p": {
+        "en": "December 2017 - March 2018",
+        "es": "Diciembre 2017 - Marzo 2018",
+    },
+    "j6t": {
+        "en": "Spa Attendant",
+        "es": "Spa Attendant",
+    },
+    "j6c": {
+        "en": "Guidance to guests through the highland spa, organization of men's spa area, guest assistance. Setting from in room dining orders and other tasks between work teams.",
+        "es": "Orientación a los huéspedes a través del spa highland, organización del área de spa para hombres, asistencia al huésped. Realización de pedidos de comidas en la habitación y otras tareas entre equipos de trabajo. ",
+    },
+    "j7p": {
+        "en": "March 2017 - November 2017",
+        "es": "Marzo 2017 - Noviembre 2017",
+    },
+    "j7c": {
+        "en": "Research on PostgreSQL database scale-out technologies, development of small utility applications in Java. Design and development of UI with HTML and CSS. ",
+        "es": "Investigación sobre tecnologías de escalamiento horizontal de bases de datos PostgreSQL, desarrollo de aplicaciones pequeñas utilitarias en Java. Diseño y desarrollo de UI con HTML y CSS.",
+    },
+    "j8p": {
+        "en": "May 2016 - August 2016",
+        "es": "Mayo 2016 - Agosto 2016",
+    },
+    "j8c": {
+        "en": "Back-up and development of procedures and functions in MySQL database. UI development with HTML and CSS for small websites with single page application format. ",
+        "es": "Back-up y desarrollo de procedimientos y funciones en base de datos MySQL. Desarrollo de UI con HTML y CSS para pequeños sitios web con formato single page application.",
+    },
+    "j8t": {
+        "en": "University internship",
+        "es": "Pasante universitario",
+    },
+    "j9p": {
+        "en": "December 2011 - February 2012",
+        "es": "Diciembre 2011 - Febrero 2012",
+    },
+    "j9t": {
+        "en": "Intern",
+        "es": "Pasante",
+    },
+    "j9c": {
+        "en": "Tareas administrativas generales. Actividades realizadas en el área de recursos humanos de la empresa.",
+        "es": "General administrative tasks. Activities carried out in the human resources area of the company.",
+    },
+    "licen": {
+        "en": "Bachelor's degree in computing sciences with emphasis in computing systems analysis",
+        "es": "Licenciado en Ciencias Informáticas con énfasis en Análisis de Sistemas Informáticos",
+    },
+    "una": {
+        "en": "National University of Asuncion - Polytechnic School",
+        "es": "Universidad Nacional de Asunción - Facultad Politécnica",
+    },
+    "grad": {
+        "en": "Graduated: 2017",
+        "es": "Graduación: 2017",
+    },
+    "uni_site": {
+        "en": "University site",
+        "es": "Sitio de la universidad",
+    },
+    "view_dip": {
+        "en": "View diploma",
+        "es": "Ver diploma",
+    },
+    "view": {
+        "en": "View",
+        "es": "Ver",
+    },
+    "c_a_d": {
+        "en": "Courses and diplomas",
+        "es": "Cursos y diplomas",
+    },
+    "c1": {
+        "en": "Computer operator - Administrative profile",
+        "es": "Operador de computadoras - Perfil administrativo",
+    },
+    "c2": {
+        "en": "Computer operator - Avdanced profile",
+        "es": "Operador de computadoras - Perfil avanzado",
+    },
+    "c3": {
+        "en": "Bachelor technician in business administration",
+        "es": "Bachiller técnico en administración de negocios",
+    },
+    "c4": {
+        "en": "Latinoware - Proof of participation",
+        "es": "Latinoware - Constancia de participación",
+    },
+    "c5": {
+        "en": "Javascript web design",
+        "es": "Diseño web javascript",
+    },
+    "c6": {
+        "en": "JQuery web design",
+        "es": "Diseño web jQuery",
+    },
+    "c7": {
+        "en": "Android programming",
+        "es": "Programación android",
+    },
+    "c8": {
+        "en": "Reports creation in iReports and Jasper Reports",
+        "es": "Creación de reportes en iReports y Jasper reports",
+    },
+}
+
+function translate(lang) {
+    let keys = Object.keys(content)
+    for (let x in keys) {
+        $("[name='" + keys[x] + "']").each(function () {
+            $(this).html(content[keys[x]][lang]);
+        });
+    }
+}
